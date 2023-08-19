96b34a476021cb9fe541e0ba0f2c68b2c4a5412e
zamu134 <117544767+zamu134@users.noreply.github.com>
2023-08-18 21:08:36 -0600

    Add files via upload

---
+++ ejercicio1.cs
@@ -0,0 +1,29 @@
+﻿int[] numeros;
+
+//realizara el pedido del tamaño del arreglo
+Console.WriteLine("ingrese el tamaño del arrreglo minimo de 5 posiciones");
+int tamaño = Convert.ToInt32(Console.ReadLine());
+
+//verificar si el tamaño es minimo de 5 posiciones 
+    if (tamaño < 5)
+    {
+        Console.WriteLine("El tamaño minimo es de 5 posiciones");
+        return;
+    }
+// se iniciara el arreglo con el tamaño dado
+    numeros = new int[tamaño];
+
+// Pedira que ingrese los valores del arreglo
+    for (int i = 0; i < numeros.Length; i++)
+    {
+        Console.Write($"Ingrese el valor {i + 1}: ");
+        numeros[i] = Convert.ToInt32(Console.ReadLine());
+    }
+//va a imprimir los valores dados
+        Console.WriteLine("Valores ingresados en el arreglo:");
+    foreach (int valor in numeros)
+    {
+        Console.WriteLine(valor);
+    }
+    
+
